1adb6b65b93eba322cfa5301fecea8e2970c41bf
shubhamshrivastavathefullstack@gmail.com <shubhamforester23@outlook.com>
2023-04-01 11:49:01 +0530

    twitter clone

--- django_forum/settings.py
+++ django_forum/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = ["*"]
 
 X_FRAME_OPTIONS = "*"
 CORS_ORIGIN_ALLOW_ALL = True
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000/"]
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000/", "https://twitter-clone-django.shubhamshriva15.repl.co"]
 
 # Application definition
 
